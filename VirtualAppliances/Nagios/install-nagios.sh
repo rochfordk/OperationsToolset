317d894021e0840d7efa2cbc02fdd357eddb8888
rochfordk <keith.rochford@gmail.com>
2014-03-31 21:29:20 +0100

    Added install and start scripts

---
+++ VirtualAppliances/Nagios/install-nagios.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+yum -y install nagios nagios-plugins-all nagios-plugins-nrpe nrpe php httpd
+
